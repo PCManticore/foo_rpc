5f853916626268acfd177c521a7096254bfcfd71
Claudiu Popa <pcmanticore@gmail.com>
2016-09-05 23:22:59 +0300

    Pass ApiResult by reference rather than value.

--- foo_rpc/api/coreversion.h
+++ foo_rpc/api/coreversion.h
@@ -9,32 +9,32 @@ namespace foobar {
     static_api_ptr_t<core_version_info_v2> core_version_2;
 
   public:
-    void get_version_string(ApiResult<const char*> result) {
+    void get_version_string(ApiResult<const char*> * result) {      
       const char * version_string = core_version_1->get_version_string();
-      result.setResult(version_string);
+      result->setResult(version_string);
     }
 
-    void get_name(ApiResult<const char*> result) {
+    void get_name(ApiResult<const char*> * result) {
       const char * name = core_version_2->get_name();
-      result.setResult(name);
+      result->setResult(name);
     }
 
-    void get_version(ApiResult<tuple<t_uint32, t_uint32, t_uint32, t_uint32>> result) {
+    void get_version(ApiResult<tuple<t_uint32, t_uint32, t_uint32, t_uint32>> * result) {
       t_core_version_data data = core_version_2->get_version();
-      result.setResult(make_tuple(data.m_major, data.m_minor1, data.m_minor2, data.m_minor3));
+      result->setResult(make_tuple(data.m_major, data.m_minor1, data.m_minor2, data.m_minor3));
     }
 
-    void get_version_as_text(ApiResult<const char *> result) {
+    void get_version_as_text(ApiResult<const char *> * result) {
       const char * version = core_version_2->get_version_as_text();
-      result.setResult(version);
+      result->setResult(version);
     }
 
     void test_version(ApiParam<tuple<t_uint32, t_uint32, t_uint32, t_uint32>> param,
-                      ApiResult<bool> result) {
+                      ApiResult<bool> * result) {
       t_uint32 major, minor1, minor2, minor3;
       tie(major, minor1, minor2, minor3) = param.value();
       bool versionCheck = core_version_2->test_version(major, minor1, minor2, minor3);
-      result.setResult(versionCheck);
+      result->setResult(versionCheck);
 
     };
   };
