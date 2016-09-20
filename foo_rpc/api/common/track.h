5ea60204744512e51e07c9ba136988d12385edb1
Claudiu Popa <pcmanticore@gmail.com>
2016-09-21 02:23:19 +0300

    Keep a reference to the metahandle_db_ptr object when creating the track.

--- foo_rpc/api/common/track.h
+++ foo_rpc/api/common/track.h
@@ -13,24 +13,28 @@ public:
   t_uint32 subsong_index;
   bool selected;
   std::string path;
+  metadb_handle_ptr handle;
 
-  static Track newTrack(t_size index, const metadb_handle_ptr & location, bool selected) {    
+  static Track newTrack(t_size index, const metadb_handle_ptr & handle, bool selected) {    
     return Track(
       index,
-      location->get_subsong_index(),
+      handle->get_subsong_index(),
       selected,
-      string(location->get_path())
+      string(handle->get_path()),
+      handle
     );
   }
 
-  Track(t_size index_value,        
+  Track(t_size index_value,
         t_uint32 subsong_index_value,
         bool selected_value,
-        std::string path_value) :
+        std::string path_value,
+        const metadb_handle_ptr & handle_value) :
     index(index_value),
     subsong_index(subsong_index_value),
     selected(selected_value),
-    path(path_value) {}
+    path(path_value),
+    handle(handle_value) {}
 
 };
 
