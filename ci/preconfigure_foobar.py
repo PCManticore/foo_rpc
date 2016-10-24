3c6542161fb13f107183b714dde8fbb3f83439a2
Claudiu Popa <pcmanticore@gmail.com>
2016-10-24 14:09:01 +0300

    Correct the preconfiguration script and use the 32-bit Python version for the GUI automation.

--- ci/preconfigure_foobar.py
+++ ci/preconfigure_foobar.py
@@ -1,10 +1,11 @@
 from pywinauto.application import Application
-from pywinauto import findwindows
 
 app = Application().start("C:\\projects\\foo-rpc\\portable_test\\foobar2000.exe /config")
 window = app.top_window_()['Tree2']
-window.GetItem(['Playback']).Click()  #Click on a top level element
-window.GetItem(['Playback', 'Output']).Click()  #Click on a sub element
+playback = window.GetItem('\\Playback')
+playback.Expand()
+output = playback.GetChild('Output')
+output.Select()
 output_page = app.top_window_()
 output_page['ComboBox'].Select('Null Output')
 output_page['OK'].Click()
