6a2488d393956a8fd00da1c1a3ac6005b946e440
Claudiu Popa <pcmanticore@gmail.com>
2016-10-24 15:17:09 +0300

    Wait for the quick appearance window and close it before changing the Output preference.

--- ci/preconfigure_foobar.py
+++ ci/preconfigure_foobar.py
@@ -1,6 +1,11 @@
 from pywinauto.application import Application
+from pywinauto import timings
 
 app = Application().start("C:\\projects\\foo-rpc\\portable_test\\foobar2000.exe /config")
+quick_config = timings.WaitUntilPasses(
+    3, 0.5, lambda: app.window_(title='Quick Appearance Setup'))
+quick_config.Close()
+
 window = app.top_window_()['Tree2']
 playback = window.GetItem('\\Playback')
 playback.Expand()
